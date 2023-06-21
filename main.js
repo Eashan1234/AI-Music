815fd607f81b4d9daed59f15ad34dbfae51bd986
Eashan1234 <132721933+Eashan1234@users.noreply.github.com>
2023-06-21 10:43:29 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 song1 = "";
 song2 = "";
 
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
+status_song1 = "";
+status_song2 = "";
+
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -12,8 +24,64 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
 }
 
 function draw() {
     image(video, 0, 0, 700, 600);
+
+    fill("#0366fc");
+    stroke("#0366fc");
+
+    status_song1 = song1.isPlaying();
+    status_song2 = song2.isPlaying();
+
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+
+        if(status_song2 == false)
+        {
+            song2.play();
+            document.getElementById("song").innerHTML = "Playing : Peter Pan Song";
+        }
+    }
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+
+        if(status_song1 == false)
+        {
+            song1.play();
+            document.getElementById("song").innerHTML = "Playing : Harry Potter Theme Song";
+        }
+    }
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
 }
